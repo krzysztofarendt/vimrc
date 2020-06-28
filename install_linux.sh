2c887b040278761c0602a9ad4ba6856f888c5b24
Krzysztof Arendt <krzysztof.arendt@gmail.com>
2020-06-28 14:12:16 +0200

    Update installation scripts

--- install_linux.sh
+++ install_linux.sh
@@ -1,2 +1,5 @@
 cp -r vim ~/.vim
+git clone https://github.com/preservim/nerdtree.git
+~/.vim/pack/vendor/start/nerdtree
+vim -u NONE -c "helptags ~/.vim/pack/vendor/start/nerdtree/doc" -c q
 cp _vimrc_linux ~/.vimrc
---
+++ install_windows.bat
@@ -0,0 +1,3 @@
+cp -r vim %USERPROFILE%\vimfiles
+git clone https://github.com/VundleVim/Vundle.vim.git %USERPROFILE%/vimfiles/bundle/Vundle.vim
+cp _vimrc_windows %USERPROFILE%\_vimrc
