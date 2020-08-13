f3de1d5eee72f7852d0f01a042cbe93afccbe148
Krzysztof Arendt <krzysztof.arendt@gmail.com>
2020-08-13 20:52:58 +0200

    Add jedi-vim to linux vim

--- install_linux.sh
+++ install_linux.sh
@@ -4,3 +4,7 @@ cp -r vim ~/.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp _vimrc_linux ~/.vimrc
 vim +PluginInstall +qall
+# Initialize jedi-vim
+cd ~/.vim/bundle/jedi-vim
+git submodule update --init --recursive
+cd ~
