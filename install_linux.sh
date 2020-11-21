77c0ca0d5c0b997c56cd1b3ef18ac3ec70608de9
Krzysztof Arendt <krzysztof.arendt@gmail.com>
2020-11-21 14:48:13 +0100

    Add YouCompleteMe

--- install_linux.sh
+++ install_linux.sh
@@ -1,10 +1,21 @@
+# Remove old VIM files
 rm -rf ~/.vim
 rm ~/.vimrc
+
+# Copy new VIM files
 cp -r vim ~/.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp _vimrc_linux ~/.vimrc
+
+# Install plugins
 vim +PluginInstall +qall
-# Initialize jedi-vim
-cd ~/.vim/bundle/jedi-vim
-git submodule update --init --recursive
+
+# Initialize jedi-vim - USING YCM NOW
+# cd ~/.vim/bundle/jedi-vim
+# git submodule update --init --recursive
+
+# Compile YCM
+cd ~/.vim/bundle/YouCompleteMe
+python3 install.py --all
+
 cd ~
--- install_windows.bat
+++ install_windows.bat
@@ -1,6 +1,16 @@
+Rem Remove previous VIM files
 rmdir %USERPROFILE%\vimfiles /s /q
 del %USERPROFILE%\_vimrc
+
+Rem Copy new VIM files
 xcopy vim %USERPROFILE%\vimfiles /s /e
 git clone https://github.com/VundleVim/Vundle.vim.git %USERPROFILE%/vimfiles/bundle/Vundle.vim
 xcopy _vimrc_windows %USERPROFILE%\_vimrc
+
+Rem Install plugins
 vim +PluginInstall +qall
+
+Rem YouCompleteMe compilation (make sure you have all dependencies and python-capable Vim)
+Rem Comment these lines if you want to switch off YCM
+cd %USERPROFILE%\vimfiles\bundle\YouCompleteMe
+python install.py --all
