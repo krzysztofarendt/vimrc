9435adc01ce13b0012d955ca62b84221803c284b
Krzysztof Arendt <krzysztof.arendt@gmail.com>
2021-10-24 20:41:56 +0200

    Add WSL config

---
+++ install_wsl.sh
@@ -0,0 +1,21 @@
+# Remove old VIM files
+rm -rf ~/.vim
+rm ~/.vimrc
+
+# Copy new VIM files
+cp -r vim ~/.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+cp _vimrc_wsl ~/.vimrc
+
+# Install plugins
+vim +PluginInstall +qall
+
+# Initialize jedi-vim
+cd ~/.vim/bundle/jedi-vim
+git submodule update --init --recursive
+
+# Compile YCM
+# cd ~/.vim/bundle/YouCompleteMe
+# python3 install.py --all
+
+cd ~
