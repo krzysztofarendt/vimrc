9eea7148da2b5e42b4e6fa4cca09e57d81a2c99e
krzysztofarendt <16005748+krzysztofarendt@users.noreply.github.com>
2023-11-10 13:03:01 +0100

    Update scripts

--- README.md
+++ README.md
@@ -11,17 +11,23 @@ Prerequisites (these commands must be in the PATH):
 
 ### Linux
 
+If you want to use YouCompleteMe, just run:
 ```
-install_linux.sh
+./install_linux.sh
 ```
+
+If you do not wish to use YouCompleteMe, you need to comment lines 17-23 in `install_linux.sh` and line 28 in `_vimrc_linux`.
 
 ### WSL (running in Windows Terminal)
 
 Almost identical as the Linux version. The differences are the cursor shape fix for Windows Terminal and a clipboard shared between WSL and Windows:
 
+If you want to use YouCompleteMe, just run:
 ```
-install_wsl.sh
+./install_wsl.sh
 ```
+
+If you do not wish to use YouCompleteMe, you need to comment lines 17-23 in `install_wsl.sh` and line 28 in `_vimrc_wsl`.
 
 ### Windows
 
--- install_linux.sh
+++ install_linux.sh
@@ -14,13 +14,13 @@ vim +PluginInstall +qall
 # https://github.com/ycm-core/YouCompleteMe#linux-64-bit
 # Uncomment below lines to install YCM
 # ===========================================================
-# sudo apt install build-essential cmake vim-nox python3-dev
-# sudo mkdir -p /etc/apt/keyrings
-# curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-# sudo echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_current.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-# sudo apt install mono-complete golang nodejs openjdk-17-jdk openjdk-17-jre npm
-# cd ~/.vim/bundle/YouCompleteMe
-# python3 install.py --all
+sudo apt install build-essential cmake vim-nox python3-dev
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+sudo echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_current.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo apt install mono-complete golang nodejs openjdk-17-jdk openjdk-17-jre npm
+cd ~/.vim/bundle/YouCompleteMe
+python3 install.py --all
 
 # Initialize jedi-vim
 # Uncomment below lines to install jedi-vim
