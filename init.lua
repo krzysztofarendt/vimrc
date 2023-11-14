2b3dfc3bdfb854ed41c533077f0b3fef9dc9dfc8
krzysztofarendt <16005748+krzysztofarendt@users.noreply.github.com>
2023-11-14 23:38:24 +0100

    Add fs command

--- init.lua
+++ init.lua
@@ -148,6 +148,7 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 -- vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fg', [[:Telescope grep_string search="" only_sort_text=true<CR>]], {})
+vim.keymap.set('n', '<leader>fs', builtin.grep_string, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
