412b03ae882b9c323190a95c4dc49b27c0b48126
krzysztofarendt <16005748+krzysztofarendt@users.noreply.github.com>
2023-11-19 22:29:51 +0100

    Deafult grep - normal (not fuzzy)

--- init.lua
+++ init.lua
@@ -146,11 +146,13 @@ require('telescope').load_extension('fzf')
 
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
--- vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fg', [[:Telescope grep_string search="" only_sort_text=true<CR>]], {})
 vim.keymap.set('n', '<leader>fs', builtin.grep_string, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+-- Grep: normal
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+-- Grep: fuzzy
+-- vim.keymap.set('n', '<leader>fg', [[:Telescope grep_string search="" only_sort_text=true<CR>]], {})
 
 -- Setup lualine --------------------------------------------------------------
 require('lualine').setup({options = {
