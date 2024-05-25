25dca3cb62c3a0468d28022f803aca48ccf551fb
krzysztofarendt <16005748+krzysztofarendt@users.noreply.github.com>
2024-05-25 12:12:02 +0200

    Re-enable <C-w> keymap in ToggleTerm

--- init.lua
+++ init.lua
@@ -133,7 +133,7 @@ require("toggleterm").setup{
 function _G.set_terminal_keymaps()
   local opts = {buffer = 0}
   vim.keymap.set('t', '<esc><esc>', [[<C-\><C-n>]], opts)
-  -- vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+  vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
 end
 
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
