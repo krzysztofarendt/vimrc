faada92a2771004b8a172404f8ab656348da7e91
krzysztofarendt <16005748+krzysztofarendt@users.noreply.github.com>
2024-05-31 20:02:42 +0200

    Add clipboard-osc52

--- init.lua
+++ init.lua
@@ -243,6 +243,19 @@ vim.keymap.set("n", "<leader>dt", [[:r! date "+\%Y-\%m-\%d \%H:\%M:\%S" <CR>]], 
 -- Other ----------------------------------------------------------------------
 vim.opt.completeopt = { "menu" }
 
+-- clipboard-osc52 ------------------------------------------------------------
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+  },
+}
+
 -- Color scheme ---------------------------------------------------------------
 vim.cmd('colorscheme rose-pine')
 -- vim.cmd('colorscheme rose-pine-dawn')
