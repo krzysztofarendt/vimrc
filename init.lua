7450f33a77ecc9b41aa0b8a5f0fe85fd3588cd44
krzysztofarendt <16005748+krzysztofarendt@users.noreply.github.com>
2024-07-21 16:49:47 +0200

    Do not show fold column

--- init.lua
+++ init.lua
@@ -138,7 +138,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
 })
 
 -- nvim-ufo -------------------------------------------------------------------
-vim.o.foldcolumn = '1' -- '0' is not bad
+vim.o.foldcolumn = '0' -- '0' means do not show fold column
 vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
