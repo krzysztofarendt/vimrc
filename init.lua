ebd747020cd457bb4dfb4cdc2b8e8cd6ae97d375
krzysztofarendt <16005748+krzysztofarendt@users.noreply.github.com>
2024-12-06 10:50:18 +0100

    Disable lua_ls by default

--- init.lua
+++ init.lua
@@ -132,7 +132,7 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities()
 local lspconfig = require('lspconfig')
 lspconfig.clangd.setup{capabilities = capabilities}
 lspconfig.pyright.setup{capabilities = capabilities}
-lspconfig.lua_ls.setup{capabilities = capabilities}
+-- lspconfig.lua_ls.setup{capabilities = capabilities}
 
 -- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
