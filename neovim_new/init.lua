ae626b1a159899aef5464063ad934f2f46e26056
Krzysztof Arendt <16005748+krzysztofarendt@users.noreply.github.com>
2025-10-12 15:54:24 +0200

    Update new NeoVim config

--- neovim_new/init.lua
+++ neovim_new/init.lua
@@ -3,16 +3,52 @@ require("config.lazy")
 vim.g.mapleader = " "
 vim.cmd.colorscheme "catppuccin"
 
+-- Cycle
+vim.keymap.set('n', '<A-Right>', '<cmd>tabnext<CR>', { desc = 'Next tab' })
+vim.keymap.set('n', '<A-Left>',  '<cmd>tabprev<CR>', { desc = 'Prev tab' })
+
+-- Faster mapping resolution
+vim.opt.timeout = true
+vim.opt.timeoutlen = 400 -- default ~1000; try 300ms (or 200)
+
 -- Tab -> 4 spaces
 vim.cmd('set tabstop=4')
 vim.cmd('set shiftwidth=4')
 vim.cmd('set expandtab')
 
+-- Line numbers
+vim.cmd('set number')
+vim.keymap.set('n', '<F5>', ':set number!<CR>', { noremap = true })
+
+-- Normal mode: clear search highlight
+vim.keymap.set('n', '<Esc><Esc>', '<cmd>nohlsearch<CR>', { silent = true, desc = 'Clear highlights' })
+
 -- Remove all trailing whitespace upon saving
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-  pattern = { "*" },
-  command = [[%s/\s\+$//e]],
+    pattern = { "*" },
+    command = [[%s/\s\+$//e]],
 })
+
+-- Terminal
+function _G.set_terminal_keymaps()
+  local opts = {buffer = 0}
+  vim.keymap.set('t', '<esc><esc>', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+end
+
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+vim.cmd('autocmd TermOpen * setlocal nonumber norelativenumber')  -- turn off line numbers
+
+----------------------- PLUGIN CONFIGURATION ----------------------------------
+
+-- Hop
+require'hop'.setup()
+local hop = require('hop')
+local directions = require('hop.hint').HintDirection
+vim.keymap.set('n', '<leader>h',  ":HopWord<CR>", {noremap = true})
+
+-- Lualine
+require('lualine').setup()
 
 -- Oil
 vim.keymap.set("n", "<leader>e", ":Oil<CR>", { desc = "Open Oil file explorer" })
@@ -27,13 +63,13 @@ vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help ta
 -- LSP configs
 vim.lsp.enable('rust_analyzer')
 vim.lsp.config('rust_analyzer', {
-  settings = {
-    ['rust-analyzer'] = {
-      diagnostics = {
-        enable = false;
-      }
+    settings = {
+        ['rust-analyzer'] = {
+            diagnostics = {
+                enable = false,
+            }
+        }
     }
-  }
 })
 
 vim.lsp.enable('pyright')
@@ -41,42 +77,53 @@ vim.lsp.enable('lua_ls')
 
 -- LSP keymaps (buffer-local on attach)
 vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('UserLspKeymaps', { clear = true }),
-  callback = function(ev)
-    local builtin = require('telescope.builtin')
-    local buf = ev.buf
-    local opts = function(desc) return { buffer = buf, silent = true, noremap = true, desc = desc } end
+    group = vim.api.nvim_create_augroup('UserLspKeymaps', { clear = true }),
+    callback = function(ev)
+        local builtin = require('telescope.builtin')
+        local buf = ev.buf
+        local opts = function(desc) return { buffer = buf, silent = true, noremap = true, desc = desc } end
 
-    -- Jumps / info
-    vim.keymap.set('n', 'gd', builtin.lsp_definitions,       opts('Go to definition'))
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration,        opts('Go to declaration'))
-    vim.keymap.set('n', 'gr', builtin.lsp_references,         opts('References'))
-    vim.keymap.set('n', 'gi', builtin.lsp_implementations,    opts('Go to implementation'))
-    vim.keymap.set('n', 'gt', builtin.lsp_type_definitions,   opts('Go to type definition'))
-    vim.keymap.set('n', '<leader>k',  vim.lsp.buf.hover,              opts('Hover'))
+        -- Jumps / info
+        vim.keymap.set('n', 'gd', builtin.lsp_definitions, opts('Go to definition'))
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts('Go to declaration'))
+        vim.keymap.set('n', 'gr', builtin.lsp_references, opts('References'))
+        vim.keymap.set('n', 'gi', builtin.lsp_implementations, opts('Go to implementation'))
+        vim.keymap.set('n', 'gt', builtin.lsp_type_definitions, opts('Go to type definition'))
+        vim.keymap.set('n', '<leader>k', vim.lsp.buf.hover, opts('Hover'))
 
-    -- Actions
-    vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename,     opts('Rename symbol'))
-    vim.keymap.set({ 'n', 'v' }, '<leader>a', vim.lsp.buf.code_action, opts('Code action'))
+        -- Actions
+        vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, opts('Rename symbol'))
+        vim.keymap.set({ 'n', 'v' }, '<leader>a', vim.lsp.buf.code_action, opts('Code action'))
 
-    -- Diagnostics
-    vim.keymap.set('n', 'gl', vim.diagnostic.open_float,      opts('Line diagnostics'))
-    vim.keymap.set('n', '[d', vim.diagnostic.goto_prev,       opts('Prev diagnostic'))
-    vim.keymap.set('n', ']d', vim.diagnostic.goto_next,       opts('Next diagnostic'))
-    vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts('Diagnostics to loclist'))
+        -- Diagnostics
+        vim.keymap.set('n', 'gl', vim.diagnostic.open_float, opts('Line diagnostics'))
+        vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts('Prev diagnostic'))
+        vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts('Next diagnostic'))
+        vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts('Diagnostics to loclist'))
 
-    -- Formatting (sync/async depending on server)
-    vim.keymap.set({ 'n', 'v' }, '<leader>f', function()
-      vim.lsp.buf.format({ async = true })
-    end, opts('Format buffer/selection'))
+        -- Formatting (sync/async depending on server)
+        vim.keymap.set({ 'n', 'v' }, '<leader>f', function()
+            vim.lsp.buf.format({ async = true })
+        end, opts('Format buffer/selection'))
 
-    -- (Optional) toggle inlay hints (Neovim 0.10+)
-    if vim.lsp.inlay_hint then
-      vim.keymap.set('n', '<leader>th', function()
-        vim.lsp.inlay_hint(buf, not vim.lsp.inlay_hint.is_enabled(buf))
-      end, opts('Toggle inlay hints'))
-    end
-  end,
+        -- (Optional) toggle inlay hints (Neovim 0.10+)
+        if vim.lsp.inlay_hint then
+            vim.keymap.set('n', '<leader>th', function()
+                vim.lsp.inlay_hint(buf, not vim.lsp.inlay_hint.is_enabled(buf))
+            end, opts('Toggle inlay hints'))
+        end
+    end,
 })
 
+-- nvim-treesitter ------------------------------------------------------------
+require 'nvim-treesitter.configs'.setup {
+    -- A list of parser names, or "all" (the listed parsers MUST always be installed)
+    ensure_installed = { "c", "cpp", "rust", "python", "lua", "markdown", "markdown_inline" },
 
+    -- Install parsers synchronously (only applied to `ensure_installed`)
+    sync_install = false,
+
+    -- Automatically install missing parsers when entering buffer
+    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+    auto_install = true,
+}
